6c5ba3cc1251150ec94c4019ddc3a8beefc288e1
诗笺 <2954700422@qq.com>
2025-02-14 18:37:05 +0800

    v1.6.7

--- www/app/redirect.js
+++ www/app/redirect.js
@@ -1,28 +1,32 @@
 'use strict';
-(function () {
-    var url = localStorage.getItem('noname_inited');
-    if (url) {
-        if (url === 'nodejs' || location.protocol.startsWith('http')) {
-            url = '';
-        }
-        var loadFailed = function () {
-            localStorage.removeItem('noname_inited');
-            window.location.reload();
-        }
-        var load = function (src, onload, onerror) {
-            var script = document.createElement('script');
-            script.src = url + 'game/' + src + '.js';
-            script.onload = onload;
-            script.onerror = onerror;
-            document.head.appendChild(script);
-        }
-        load('update', function () {
-            load('config', function () {
-                load('package', function () {
-                    load('game', null, loadFailed);
-                }, loadFailed);
-            }, loadFailed);
-        }, loadFailed);
-        window.cordovaLoadTimeout = setTimeout(loadFailed, 10000);
-    }
-}());
+(async () => {
+	let url = localStorage.getItem('noname_inited');
+
+	if (!url) return;
+
+	if (url === 'nodejs' || location.protocol.startsWith('http')) url = '';
+
+	const loadFailed = () => {
+		localStorage.removeItem('noname_inited');
+		window.location.reload();
+	};
+	const load = src => new Promise((resolve, reject) => {
+		const script = document.createElement('script');
+		script.src = `${url}game/${src}.js`;
+		script.onload = resolve;
+		script.onerror = reject;
+		document.head.appendChild(script);
+	});
+	window.cordovaLoadTimeout = setTimeout(loadFailed, 10000);
+
+	try {
+		await Promise.all([
+			load('update'),
+			load('config'),
+			load('package')
+		]);
+		await load('game');
+	} catch {
+		loadFailed();
+	}
+})();
